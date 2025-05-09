c058731198018ac387b117714de1e388b07acbcc
BahaaShk <bahaa.shk@gmail.com>
2025-05-10 00:35:04 +0300

    few additions to the cart page

--- src/components/cart-card/cartCard.jsx
+++ src/components/cart-card/cartCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { RxTrash } from 'react-icons/rx'
 
-const cartCard = () => {
+const CartCard = ({product}) => {
+  const {name, price , description, imageURL} = product
+  const handleDelete = () => {
+    
+  }
   return (
-    <div>cartCard</div>
+  
+      <div className='cart-card'>
+        <img src={imageURL} alt={name} className='cart-card__image' />
+        <span className='cart-card__title'>{name}</span>
+        <span className='cart-card__description'>{description}</span>
+        <p>{price}</p>
+        <RxTrash onClick={handleDelete} className='cart-card__icon' />
+      </div>
+  
   )
 }
 
-export default cartCard
+export default CartCard
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { products } from "../utils/products";
+import CartCard from "../components/cart-card/cartCard";
+import React from "react";
 
 export default function Cart() {
   return (
-    <div>cart</div>
-  )
+    <div className="cart">
+      <div className="cart__products">
+        {
+          products.map((product) => {
+          return <CartCard product={product} key={product.id} />
+          })
+        }
+      </div>
+      <div className="cart__checkout">
+        <h1>Checkout</h1>
+        <h2>Username: </h2>
+        <h2> Total:$999</h2>
+        <button>Pay</button>
+      </div>
+    </div>
+  );
 }
